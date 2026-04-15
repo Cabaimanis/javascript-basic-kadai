03bea1e748c3163ce28f8effb8a9f7f87aecbf4d
Cabaimanis <yusui.tokachi1104@gmail.com>
2026-04-15 11:41:19 +0900

    Update calc.js

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -4,20 +4,20 @@ let num = 30
 
 //もし変数numが3と5の倍数ならば “3と5の倍数です”と出力する
 if (num % 3 === 0 && num % 5 === 0) {
-  console.log('numは3と5の倍数です');
+  console.log('3と5の倍数です');
 }
 
 //もし変数numが3の倍数ならば “3の倍数です”と出力する
 else if (num % 3 === 0) {
-  console.log('numは3の倍数です');
+  console.log('3の倍数です');
 }
 
 //もし変数numが5の倍数ならば “5の倍数です”と出力する
 else if (num % 5 === 0) {
-  console.log('numは5の倍数です');
+  console.log('5の倍数です');
 }
 
 //それ以外の場合は “num”と出力する
 else {
-  console.log('numです');
+  console.log(num);
 }
